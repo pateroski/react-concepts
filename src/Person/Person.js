30e75f8615100fb5b21b0fc7fbb96bc5959b4bfa
Andrés Reyes <p4ter0ski@gmail.com>
2019-09-30 09:55:14 +0200

    Revert "[feat] add ErrorBoundary component to control unpredictable errors from components and show info to the user"
    This reverts commit c5836d37d45b6ab6a4100e12d433930cec6fc813.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,11 +6,6 @@ import classes from './Person.css';
 //Input has two way binding. onChange invokes whatever function is added on
 //changed props and value is received from parent from name prop
 const person = (props) => {
-  const errorSpot = Math.random();
-
-  if (errorSpot > 0.7) {
-    throw new Error('something went wrong!');
-  }
   return (
     <section className={classes.Person}>
       <p onClick={props.click}>
