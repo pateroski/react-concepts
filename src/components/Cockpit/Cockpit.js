0c07d1c92a02964fcea28441b63aa92616c1451f
Andrés Reyes <p4ter0ski@gmail.com>
2019-10-09 20:42:29 +0200

    [feat] add useEffect hooks for adding support to functional compoments to use the more important lifecycle hooks: CompomentDidMount and CompomentDidUpdate

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+
+  /**
+   * This is how you manage main lifecycle hooks with
+   * functional components.
+   *
+   * Actually the most important lifecycle hooks which
+   * are componentDidMount and componentDidUpdate (the
+   * final ones for each phase, creation and update),
+   * can be combined in only one effect.
+   *
+   * 1) useEffect is called every time a render cycle is
+   * done in the compoment. Check it out.
+   */
+  // useEffect(() => {
+  //   console.log('[Cockpit.js] useEffect');
+  // })
+
+  /**
+   * 2) What happens if always runs on every rendery cycle?
+   * How we control when useEffect should be called?
+   */
+  // useEffect(() => {
+  //   console.log('[Cockpit.js] useEffect');
+  //   setTimeout(() => {
+  //     alert('data saved to cloud!')
+  //   }, 1000);
+  // })
+
+  /**
+   * For that, you can specify when useEffect should change
+   * as a second parameter (first one is the function). Everything
+   * which makes useEffect being called can be passed as an argument
+   * Toggle the Toggle persons button and check what happens after the
+   * first cycle render
+   */
+  // useEffect(() => {
+  //   console.log('[Cockpit.js] useEffect');
+  //   setTimeout(() => {
+  //     alert('data saved to cloud!')
+  //   }, 1000);
+  // }, [props.persons])
+
+  /**
+   * 3) What if we only want to run the useEffect hook the first
+   * render cycle?
+   *
+   * Well, in that case, yo should not pass any dependency which
+   * makes calls useEffect each time the render cycle is called
+   *
+   * Important!: btw, you can have as many useEffect as you want.
+   */
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    setTimeout(() => {
+      alert('data saved to cloud!')
+    }, 1000);
+  }, [])
+
+
   const paragraphClassList = [];
   let btnClass = null;
 
